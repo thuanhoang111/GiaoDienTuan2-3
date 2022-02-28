0caef9f9b47a1433eb3de2f26cb9d216451c17f9
Your Name <you@example.com>
2022-02-28 16:41:29 +0700

    finish giao diện Tuan 3

---
+++ giao diện tuần 2,3/app/src/main/java/com/example/myapplication/GiaoDien_danhGiaSp.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GiaoDien_danhGiaSp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giao_dien_danh_gia_sp);
+    }
+}
---
+++ giao diện tuần 2,3/app/src/main/java/com/example/myapplication/giaoDiemReviewSP.java
@@ -0,0 +1,14 @@
+                                                                                                                                                                                                                                                                                                                                                                                                      package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class giaoDiemReviewSP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giao_diem_review_s_p);
+    }
+}
